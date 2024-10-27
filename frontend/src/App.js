cdb36dd9ac82b71c5eb0e5e664ef4638f9a9f136
Maxim Rudaites <rudajtes@gmail.com>
2024-10-27 09:15:03 +0200

    Update fetch in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     // Виконуємо запит до нашого бекенду
-    fetch('http://localhost:5000/api/message')
+    fetch('http://159.65.44.118:5000/api/message')
       .then((response) => response.json()) // Перетворюємо відповідь у JSON
       .then((data) => setMessage(data.message)) // Зберігаємо отримане повідомлення у стані
       .catch((error) => console.error('Error fetching message:', error)); // Ловимо помилки, якщо є
